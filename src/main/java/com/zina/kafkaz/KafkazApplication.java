3e0b966c8e3dac5fca828341e5a1979bc5fcffe3
Lacina ZINA <zinalacina@gmail.com>
2020-12-28 03:46:55 -0600

    First commit

---
+++ src/main/java/com/zina/kafkaz/KafkazApplication.java
@@ -0,0 +1,13 @@
+package com.zina.kafkaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkazApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkazApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zina/kafkaz/KafkazApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zina.kafkaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkazApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
